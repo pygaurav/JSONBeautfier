8085c6f85e925e44d8d27ada922b0268c0e51cca
Gaurav Bhardwaj <smartgaurav22@gmail.com>
2019-06-25 15:22:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -35,8 +35,8 @@ def index():
                 ErrorName=""
         else:
             ErrorName="Please Enter Something"
-        
-    return render_template('response.html',req_text=req_text,res_text=res_text,ErrorName=ErrorName)
+    if(!HasError):    
+        return render_template('response.html',req_text=req_text,res_text=res_text,ErrorName=ErrorName)
 
 
 def formatJson(req_text,res_text):
